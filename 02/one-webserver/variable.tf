28fb3c677c6aeec48901381445cde3a5d40b5bf3
jungyeon96 <0906.wjddus@gmail.com>
2024-05-29 17:19:29 +0900

    update

--- 02/one-webserver/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "mySGname" {
-  default     = "allow_WEB"
-  description = "보안 그룹 이름"
-  type        = string
-}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Output declarations
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID"
+}
+
+output "lb_url" {
+  value       = "httpd://${module.elb_http.elb_dns_name}"
+  description = "ELB URL"
+}
+
+output "web_server_count" {
+  value       = length(module.ec2_instances.instance_ids)
+  description = "Number of web servers provisioned"
+
+}
